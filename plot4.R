e84b340ffd38188dcff6ef354e1490c8d08c5e78
francoisgirouard <francoisgirouard@videotron.ca>
2014-10-10 20:49:56 -0400

    Adjusted plot4 (removed box around legend)

--- plot4.R
+++ plot4.R
@@ -48,10 +48,11 @@ legend(x = "topright",
        legend = c("Sub_metering_1",
                   "Sub_metering_2",
                   "Sub_metering_3"),
-       lty = c(1, 1, 1),
        col = c("black",
                "red",
-               "blue"))
+               "blue"),
+       lty = c(1, 1, 1),
+       bty = "n")
 
 plot(x = data$DateTime,
      y = data$Global_reactive_power,
